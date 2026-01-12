2277da4632a6e7b55b58ba061ab7d1d14bd48c58
BioHazard786 <bzatch70@gmail.com>
2026-01-12 16:08:46 +0530

    Add metadata for QR to UPI app: long description, short description, and title
    - Created long_description.txt to explain the app's functionality and benefits.
    - Added short_description.txt for a concise overview of the app.
    - Introduced title.txt for the app's name.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -24,8 +24,8 @@ android {
         applicationId = "com.biohazard786.qrtoupi"
         minSdk = 24
         targetSdk = 36
-        versionCode = 1
-        versionName = "0.0.1"
+        versionCode = 2
+        versionName = "0.0.2"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
